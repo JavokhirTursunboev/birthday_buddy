26777d141e0b8f3ebdba153b79460ca8e9a7c302
Javokhir <jtursunboev@bk.ru>
2023-06-05 03:51:42 +0900

    css changed

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -4,7 +4,7 @@ const Person = (props) => {
   return (
     <div className="person">
       <img src={props.image} alt={props.name} className="image" />
-      <div className="text">
+      <div >
         <h4>{props.name}</h4>
         <p>{props.age} years</p>
       </div>
